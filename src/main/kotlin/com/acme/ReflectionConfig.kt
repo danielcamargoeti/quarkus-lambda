9559fc128f5280b9d5bdbab8765c2d1142723825
Daniel Teleginski Camargo <daniel.camargo.eti@churchofjesuschrist.org>
2022-09-28 14:38:16 -0600

    wip

--- src/main/kotlin/com/acme/ReflectionConfig.kt
+++ src/main/kotlin/com/acme/ReflectionConfig.kt
@@ -1,11 +1,15 @@
 package com.acme
 
 import com.google.api.client.auth.oauth2.*
+import com.google.api.client.auth.openidconnect.IdToken
+import com.google.api.client.googleapis.auth.oauth2.*
+import com.google.api.client.json.webtoken.JsonWebToken
 import io.quarkus.runtime.annotations.RegisterForReflection
 
 @Suppress("unused")
 @RegisterForReflection(
     targets = [
+        // Google OAuth client
         AuthorizationCodeRequestUrl::class,
         AuthorizationCodeResponseUrl::class,
         AuthorizationCodeTokenRequest::class,
@@ -14,7 +18,16 @@ import io.quarkus.runtime.annotations.RegisterForReflection
         RefreshTokenRequest::class,
         TokenErrorResponse::class,
         TokenRequest::class,
-        TokenResponse::class
+        TokenResponse::class,
+        // Google api client
+        GoogleAuthorizationCodeRequestUrl::class,
+        GoogleBrowserClientRequestUrl::class,
+        GoogleClientSecrets::class,
+        GoogleClientSecrets.Details::class,
+        GoogleIdToken.Payload::class,
+        GoogleTokenResponse::class,
+        IdToken.Payload::class,
+        JsonWebToken.Payload::class
     ]
 )
 class ReflectionConfig
